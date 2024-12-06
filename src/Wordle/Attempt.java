c0e1e02058ac0a1cfc82ec80aca13d924aa07bac
zuzaanatolie <anatolie.zuza@student.kdg.be>
2024-12-06 15:41:19 +0100

    Generate Feedback checks if the character is in the secret word, and makes a string of all the characters allready colored.

--- src/Wordle/Attempt.java
+++ src/Wordle/Attempt.java
@@ -4,10 +4,38 @@ import java.util.List;
 
 public class Attempt {
     protected String guess;
+    private final StringBuilder guessFeedback = new StringBuilder();
     private List<CharacterFeedback> feedback;
 
-    public Attempt(String guess) {
+    public Attempt(String guess, String secretWord) {
         setGuess(guess);
+    }
+
+    public String generateFeedback(String guess, String secretWord) {
+        for (int i = 0; i < 5; i++) {
+            char currentChar = guess.charAt(i);
+            StringBuilder characterFeedback = new StringBuilder();
+            String green = "\033[32m";
+            String yellow = "\033[93m";
+            String reset = "\033[0m";
+            boolean isCorrectPosition = currentChar == secretWord.charAt(i);
+            boolean isPresentInWord = secretWord.contains(String.valueOf(currentChar));
+            if (isCorrectPosition) {
+                characterFeedback.append(green).append(currentChar).append(reset);
+            } else if (isPresentInWord) {
+                characterFeedback.append(yellow).append(currentChar).append(reset);
+            } else if(currentChar >= 'A' && currentChar <= 'Z') {
+                characterFeedback.append(currentChar);
+            }
+            guessFeedback.append(characterFeedback);
+        }
+        System.out.println(guessFeedback);
+        guessFeedback.setLength(0);
+        return guessFeedback.toString();
+    }
+
+    public boolean isCorrect(String secretWord) {
+        return guess.equals(secretWord);
     }
 
     public void setGuess(String guess) {
@@ -17,14 +45,4 @@ public class Attempt {
     public String getGuess() {
         return guess;
     }
-
-    public boolean isCorrect(String guess) {
-        boolean isValid = false;
-        if (guess.length() == 5) {
-            isValid = true;
-        }
-        return isValid;
-        //deeez nuts
-    }
-
 }
